ddd1585447a5845171cbc4e00b46d7381d0e6726
Pranava77 <pranavarholla@gmail.com>
2023-10-29 14:14:40 +0530

    first message

---
+++ script.js
@@ -0,0 +1,29 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main1'),
+    smooth: true
+});
+
+
+var tl = gsap.timeline()
+
+tl.to("#main1",{
+    y:"100vh" ,
+    duration:0.2,
+    scale: 0.4
+})
+
+
+tl.to("#main1",{
+    y:"vh" ,
+    duration:1,
+    Delay: 0.2
+})
+
+
+tl.to("#main1",{
+   rotate: 360,
+   scale:1,
+   duration: 1
+})
+
+
